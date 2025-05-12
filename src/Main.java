c6d6bbd9ef097bc672ef4ee335d6eaa1d0ca4824
Bion Ullenius <Bion.Holmberullenius@elev.ga.ntig.se>
2025-05-12 10:43:22 +0200

    färdig med servern

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.net.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String args[])
+            throws Exception {
+
+        // Default port number we are going to use
+        int portnumber = 55050;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
         }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticastSocket =
+                new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+        // Determine the IP address of a host, given the host name
+        InetAddress group =
+                InetAddress.getByName("225.4.5.6");
+
+        // getByName- returns IP address of given host
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        // Continually receives data and prints them
+        while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data =
+                    new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg =
+                    new String(data.getData()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
 }
